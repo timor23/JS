610f419320ff54bd44964aed27e0f68dc223e1be
timor23 <timors23@protonmail.com>
2021-09-12 12:47:29 +0300

    2.1-sol

---
+++ 2.1-functions/2.1-functions.js
@@ -0,0 +1,11 @@
+function describeCountry(country, population, capitalCity) {
+    return country + ' has ' + population + " people and it's capital city is " + capitalCity;
+}
+
+let finland = describeCountry('Finland', '6 million', 'Helsinky');
+let indonesia = describeCountry('Indonesia', '270 million', 'Jakarta');
+let iceland = describeCountry('Iceland', '368 thousands', 'Reykjavík');
+
+// console.log(finland);
+// console.log(indonesia);
+// console.log(iceland);
