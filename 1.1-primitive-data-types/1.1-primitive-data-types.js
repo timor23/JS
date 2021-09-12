9f061fc8d8f28cb8b90a2c60552d920b914c93cb
timor23 <timors23@protonmail.com>
2021-09-12 10:40:37 +0300

    1.1-sol

---
+++ 1.1-primitive-data-types/1.1-primitive-data-types.js
@@ -0,0 +1,9 @@
+let str1 = "Welcome Home!!!";
+let num1 = 117;
+let person = {name:"Timor", age:35, height:"179cm"}; //that's me
+let yes = true;
+let nothing = '';
+let undefined;
+let symbol = '&';
+
+// console.log(person.name);
--- ex1/Ex1.js
+++
@@ -1,7 +0,0 @@
-let arr1 = [];
-
-arr1.push(10);
-arr1.push(20);
-arr1.push(30);
-
-console.log(arr1[2]);
