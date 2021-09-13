5139bbe78bd089b46a6d0f96014014e75db6eb29
timor23 <timors23@protonmail.com>
2021-09-13 15:33:49 +0300

    6.1 sol

---
+++ 6.1-array-numbers/6.1-array-numbers.js
@@ -0,0 +1,36 @@
+const arr = [1, 7, 3, 1, -5, 7, 3, 9];
+
+function printArray(arr) {
+    for (let i = 0 ; i < arr.length ; i++) {
+        console.log(arr[i]);
+    }
+}
+
+function arrayLength(arr) {
+    let lengh = 0;
+    arr.forEach(element => {
+        lengh++;
+    });
+    return lengh;
+}
+
+function arraySum(arr) {
+    let sum = 0;
+    arr.forEach(val => {
+        sum += val;
+    });
+    return sum;
+}
+
+function arrayMulti(arr) {
+    let mul = 1;
+    arr.forEach(val => {
+        mul *= val;
+    });
+    return mul;
+}
+
+
+console.log(`Array arr[] length is ${arrayLength(arr)}`);
+console.log(`Array arr[] elements sum is ${arraySum(arr)}`);
+console.log(`Array arr[] elements multipication is ${arrayMulti(arr)}`);
--- test.js
+++ test.js
@@ -1,2 +1,1 @@
-let sign ='lllopp';
-sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
+no
