efa0854be4ab9b5bbab988bf31914c7affc61e7b
timor23 <timors23@protonmail.com>
2021-09-19 15:10:25 +0300

    10.1

---
+++ 10.1-map-forEach/index.js
@@ -0,0 +1,57 @@
+const newReleases = [
+    {
+        id: 70111470,
+        title: "Die Hard",
+        boxart: "http://cdn-0.nflximg.com/images/2891/DieHard.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [4.0],
+        bookmark: [],
+    },
+    {
+        id: 654356453,
+        title: "Bad Boys",
+        boxart: "http://cdn-0.nflximg.com/images/2891/BadBoys.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [5.0],
+        bookmark: [{ id: 432534, time: 65876586 }],
+    },
+    {
+        id: 65432445,
+        title: "The Chamber",
+        boxart: "http://cdn-0.nflximg.com/images/2891/TheChamber.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [4.0],
+        bookmark: [],
+    },
+    {
+        id: 675465,
+        title: "Fracture",
+        boxart: "http://cdn-0.nflximg.com/images/2891/Fracture.jpg",
+        uri: "http://api.netflix.com/catalog/titles/movies/70111470",
+        rating: [5.0],
+        bookmark: [{ id: 432534, time: 65876586 }],
+    },
+];
+
+function allMovies(arr) {
+    ret = [];
+    arr.forEach(element => {
+        ret.push({id: element.id, title: element.title});
+    });
+    return ret;
+}
+
+function allMoviesMap(arr) {
+    ret = new Map;
+    arr.forEach((element) => {
+        ret.set({id: element.id, title: element.title})
+    });
+    return ret;
+}
+
+
+
+let r = allMovies(newReleases);
+console.log(r);
+let s = allMoviesMap(newReleases);
+console.log(s);
