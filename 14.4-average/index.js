5c22dd1f0c1017f866195614b2bf9729457cb6a3
timor23 <timors23@protonmail.com>
2021-09-22 01:08:13 +0300

    14.4

---
+++ 14.4-average/index.js
@@ -0,0 +1,10 @@
+function calcAverage (arr){
+    let sum = 0;
+    for  (let i = 0 ; i < arr.length ; i++){
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+let r = calcAverage  ([85, 90, 92]);
+console.log(r);
+
