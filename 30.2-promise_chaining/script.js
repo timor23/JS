5f14d69eda3b9a1b6e3a5875755e908fc497eef8
timor23 <timors23@protonmail.com>
2021-10-04 11:11:13 +0300

    30.1

---
+++ 30.2-promise_chaining/script.js
@@ -0,0 +1,27 @@
+const wordsArr = ["sound", "week", "minute", "fan", "explode"];
+const wordsArr2 = ["pray", "flower", "duty", "wound", 88];
+function makeAllCaps(words) {
+    return new Promise(((resolve, reject) => {
+        if (words.every((word) => typeof word === 'string')) {
+            resolve(words.map((word) => word.toUpperCase()));
+        } else {
+            reject("The array contains one or more none string elements!");
+        }
+    })).then((words) => sortWords(words));
+}
+
+function sortWords(words) {
+    return words.sort();
+}
+
+makeAllCaps(wordsArr).then((request) => {
+    console.log(request)
+}).catch((err) => {
+    console.log(err)
+});
+
+makeAllCaps(wordsArr2).then((request) => {
+    console.log(request)
+}).catch((err) => {
+    console.log(err)
+});
